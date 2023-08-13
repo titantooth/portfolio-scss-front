062740f4b3d4671ab84dfff934344399d5207c2e
titantooth <ali.rabieeie@gmail.com>
2023-08-13 16:12:49 +0330

    main structure done

---
+++ portfolio2/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src={images.logo} alt="image logo" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
