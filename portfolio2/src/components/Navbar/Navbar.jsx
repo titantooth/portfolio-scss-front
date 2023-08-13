ab43c03d4b101f680e1b85b3e9b620db63cafc20
titantooth <ali.rabieeie@gmail.com>
2023-08-13 16:38:35 +0330

    navbar minus hamburger

--- portfolio2/src/components/Navbar/Navbar.jsx
+++ portfolio2/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "./Navbar.scss";
+import { images } from "../../constants";
 
 const Navbar = () => {
+  const navNames = ["home", "about", "work", "skills", "contact"];
   return (
-    <nav>
-      <div>
+    <nav className="app-navbar">
+      <div className="app-navbar-logo">
         <img src={images.logo} alt="image logo" />
       </div>
+      <ul className="app-navbar-links">
+        {navNames.map((item) => (
+          <li key={`link-${item}`} className="app-flex p-text">
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
