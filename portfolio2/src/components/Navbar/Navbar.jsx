eaebbb1dbbd3a2a201891552cf204a9ad1d617a9
titantooth <ali.rabieeie@gmail.com>
2023-08-13 20:52:45 +0330

    finished navbar and styling

--- portfolio2/src/components/Navbar/Navbar.jsx
+++ portfolio2/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./Navbar.scss";
 import { images } from "../../constants";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { easeOut, motion } from "framer-motion";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   const navNames = ["home", "about", "work", "skills", "contact"];
   return (
     <nav className="app-navbar">
       <div className="app-navbar-logo">
-        <img src={images.logo} alt="image logo" />
+        <h1>RABIEE</h1>
+        {/* <img src={images.logo} alt="image logo" /> */}
       </div>
       <ul className="app-navbar-links">
         {navNames.map((item) => (
@@ -17,6 +23,27 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className="app-navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: easeOut }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul className="app-navbar-links">
+              {navNames.map((item) => (
+                <li key={item}>
+                  <a onClick={() => setToggle(false)} href={`#${item}`}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 };
