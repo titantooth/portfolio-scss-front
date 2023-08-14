4c45c3d3ab9c3ed5509a36d2063722ad8b65bfbf
titantooth <ali.rabieeie@gmail.com>
2023-08-14 14:22:51 +0330

    finished the header component not much css

--- portfolio2/src/container/Header/Header.jsx
+++ portfolio2/src/container/Header/Header.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import "./Header.scss";
-import { motion } from "framer-motion";
+import { easeInOut, motion } from "framer-motion";
 import { images } from "../../constants";
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+};
 
 const Header = () => {
   return (
-    <div className="app-header app-flex">
+    <div id="home" className="app-header app-flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
@@ -19,7 +30,36 @@ const Header = () => {
               <h1 className="head-text">Soroosh</h1>
             </div>
           </div>
+          <div className="tag-cmp app-flex">
+            <p className="p-text">Web Developper </p>
+            <p className="p-text">UI/UX Designer </p>
+          </div>
         </div>
+      </motion.div>
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app-header-img"
+      >
+        <img src={images.profile} alt="profile.bg" />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="overlay-circle"
+          src={images.circle}
+          alt="image circle"
+        ></motion.img>
+      </motion.div>
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app-header-circles"
+      >
+        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+          <div className="circle-cmp app-flex" key={`circle-${index}`}>
+            <img src={circle} alt="circle" />
+          </div>
+        ))}
       </motion.div>
     </div>
   );
