8aeb35e755c4355f1210ead5fd942bceb3809779
titantooth <ali.rabieeie@gmail.com>
2023-08-16 21:30:20 +0330

    finished footer

--- portfolio2/src/container/Footer/Footer.jsx
+++ portfolio2/src/container/Footer/Footer.jsx
@@ -1,8 +1,93 @@
 import React from "react";
 import "./Footer.scss";
+import { images } from "../../constants";
+import { useState } from "react";
+import { appWrap, motionWrap } from "../../wrapper";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { name, email, message } = formData;
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    setLoading(true);
+    const contact = {
+      type: "contact",
+      name: name,
+      email: email,
+      message: message,
+    };
+  };
+
+  return (
+    <>
+      <h2 className="head-text">Take a coffee & let's meet</h2>
+      <div className="app-footer-cards">
+        <div className="app-footer-card">
+          <img src={images.email} alt="email" />
+          <a href="mailto:ali.rabieeie@gmail.com" className="p-text">
+            ali.rabieeie@gmail.com
+          </a>
+        </div>
+        <div className="app-footer-card">
+          <img src={images.mobile} alt="mobile" />
+          <a href="tel: +989304739047" className="p-text">
+            +98 930 473 9047
+          </a>
+        </div>
+      </div>
+
+      <div className="app-footer-form app-flex">
+        <div className="app-flex">
+          <input
+            name="name"
+            type="text"
+            className="p-text"
+            placeholder="Your Name"
+            value={name}
+            onChange={handleChangeInput}
+          />
+        </div>
+        <div className="app-flex">
+          <input
+            name="email"
+            type="email"
+            className="p-text"
+            placeholder="Your E-mail"
+            value={email}
+            onChange={handleChangeInput}
+          />
+        </div>
+        <div>
+          <textarea
+            className="p-text"
+            placeholder="Your Message"
+            value={message}
+            name="message"
+            onChange={handleChangeInput}
+          ></textarea>
+        </div>
+
+        <button className="p-text" type="button" onClick={handleSubmit}>
+          {loading ? "Sending" : "Send"}
+        </button>
+      </div>
+    </>
+  );
 };
 
-export default Footer;
+export default appWrap(
+  motionWrap(Footer, "app-footer"),
+  "contact",
+  "app-whitebg"
+);
--- portfolio2/src/container/Testimonial/Testimonial.jsx
+++ portfolio2/src/container/Testimonial/Testimonial.jsx
@@ -1,8 +1,81 @@
 import React from "react";
 import "./Testimonial.scss";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+import {
+  HiChevronLeft,
+  HiChevronDoubleRight,
+  HiChevronRight,
+} from "react-icons/hi";
+import { appWrap, motionWrap } from "../../wrapper";
+import { useState, useEffect } from "react";
 
+const brand = [{}];
+const testimonial = [
+  {
+    name: "X",
+    company: "Y",
+    imgUrl: images.profile,
+    feedback: "Soroosh is an awesome developer",
+  },
+  {
+    name: "XY",
+    company: "YX",
+    imgUrl: images.amazon,
+    feedback: "Soroosh is an awesome designer",
+  },
+];
 const Testimonial = () => {
-  return <div>Testimonial</div>;
+  const [brands, setBrands] = useState(brand);
+  const [testimonials, setTestimonials] = useState(testimonial);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+  };
+
+  return (
+    <>
+      <div className="app-testimonial-item app-flex">
+        <img
+          src={testimonials[currentIndex].imgUrl}
+          alt="testimonial picture"
+        />
+        <div className="app-testimonial-content">
+          <p className="p-text">{testimonials[currentIndex].feedback}</p>
+          <div>
+            <h4 className="bold-text">{testimonials[currentIndex].name}</h4>
+            <h5 className="p-text">{testimonials[currentIndex].company}</h5>
+          </div>
+        </div>
+      </div>
+      <div className="app-testimonial-btns app-flex">
+        <div
+          className="app-flex"
+          onClick={() =>
+            handleClick(
+              currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1
+            )
+          }
+        >
+          <HiChevronLeft />
+        </div>
+        <div
+          className="app-flex"
+          onClick={() =>
+            handleClick(
+              currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1
+            )
+          }
+        >
+          <HiChevronRight />
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default Testimonial;
+export default appWrap(
+  motionWrap(Testimonial, "app-testimonial"),
+  "testimonials"
+);
